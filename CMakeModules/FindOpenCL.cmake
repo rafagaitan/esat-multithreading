efdb765616dfab34dd8e1746073b600b2d383f93
Rafael Gaitán <rafa.gaitan@mirage-tech.com>
2016-04-07 22:41:22 +0200

    Fixed build on macosx for opencl, disabled CUDA, has tons of conflicts with c++11

--- CMakeModules/FindOpenCL.cmake
+++ CMakeModules/FindOpenCL.cmake
@@ -1,68 +1,136 @@
-FIND_PATH(OPENCL_INCLUDE_DIR
-	NAMES
-		CL/cl.h OpenCL/cl.h
-	PATHS
-		$ENV{AMDAPPSDKROOT}/include
-		$ENV{INTELOCLSDKROOT}/include
-		$ENV{NVSDKCOMPUTE_ROOT}/OpenCL/common/inc
-		$ENV{NVSDKCOMPUTE_ROOT}/include
-		# Legacy Stream SDK
-		$ENV{ATISTREAMSDKROOT}/include
-		$ENV{CUDA_PATH}/include
-		$ENV{OPENCL_DIR}/include
-		$ENV{OPENCL_ROOT}/include
-		$ENV{CUDA_PATH_V6_5}/include
-)
-		
-IF(CMAKE_SIZEOF_VOID_P EQUAL 4)
-	SET(OPENCL_LIB_SEARCH_PATH
-		${OPENCL_LIB_SEARCH_PATH}
-		$ENV{AMDAPPSDKROOT}/lib/x86
-		$ENV{INTELOCLSDKROOT}/lib/x86
-		$ENV{NVSDKCOMPUTE_ROOT}/OpenCL/common/lib/Win32
-		$ENV{NVSDKCOMPUTE_ROOT}/lib/Win32
-		$ENV{CUDA_PATH}/lib/Win32
-		$ENV{OPENCL_DIR}/lib/Win32
-		$ENV{OPENCL_ROOT}/lib/Win32
-		$ENV{CUDA_PATH_V6_5}/lib/Win32
-		# Legacy Stream SDK
-		$ENV{ATISTREAMSDKROOT}/lib/x86
-)
-ELSEIF(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  SET(OPENCL_LIB_SEARCH_PATH
-		${OPENCL_LIB_SEARCH_PATH}
-		$ENV{AMDAPPSDKROOT}/lib/x86_64
-		$ENV{INTELOCLSDKROOT}/lib/x64
-		$ENV{NVSDKCOMPUTE_ROOT}/OpenCL/common/lib/x64
-		$ENV{NVSDKCOMPUTE_ROOT}/lib/x64
-		$ENV{CUDA_PATH}/lib/x64
-		$ENV{OPENCL_DIR}/lib/x64
-		$ENV{OPENCL_ROOT}/lib/x64
-		$ENV{CUDA_PATH_V6_5}/lib/x64
-		# Legacy stream SDK
-		$ENV{ATISTREAMSDKROOT}/lib/x86_64
-)
-ENDIF(CMAKE_SIZEOF_VOID_P EQUAL 4)
+#.rst:
+# FindOpenCL
+# ----------
+#
+# Try to find OpenCL
+#
+# Once done this will define::
+#
+#   OpenCL_FOUND          - True if OpenCL was found
+#   OpenCL_INCLUDE_DIRS   - include directories for OpenCL
+#   OpenCL_LIBRARIES      - link against this library to use OpenCL
+#   OpenCL_VERSION_STRING - Highest supported OpenCL version (eg. 1.2)
+#   OpenCL_VERSION_MAJOR  - The major version of the OpenCL implementation
+#   OpenCL_VERSION_MINOR  - The minor version of the OpenCL implementation
+#
+# The module will also define two cache variables::
+#
+#   OpenCL_INCLUDE_DIR    - the OpenCL include directory
+#   OpenCL_LIBRARY        - the path to the OpenCL library
+#
 
-FIND_LIBRARY(
-    OPENCL_LIBRARY
-    NAMES OpenCL
-    PATHS ${OPENCL_LIB_SEARCH_PATH})
+#=============================================================================
+# Copyright 2014 Matthaeus G. Chajdas
+#
+# Distributed under the OSI-approved BSD License (the "License");
+# see accompanying file Copyright.txt for details.
+#
+# This software is distributed WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the License for more information.
+#=============================================================================
+# (To distribute this file outside of CMake, substitute the full
+#  License text for the above reference.)
+
+function(_FIND_OPENCL_VERSION)
+  include(CheckSymbolExists)
+  include(CMakePushCheckState)
+  set(CMAKE_REQUIRED_QUIET ${OpenCL_FIND_QUIETLY})
+
+  CMAKE_PUSH_CHECK_STATE()
+  foreach(VERSION "2_0" "1_2" "1_1" "1_0")
+    set(CMAKE_REQUIRED_INCLUDES "${OpenCL_INCLUDE_DIR}")
+
+    if(APPLE)
+      CHECK_SYMBOL_EXISTS(
+        CL_VERSION_${VERSION}
+        "${OpenCL_INCLUDE_DIR}/Headers/OpenCL/cl.h"
+        OPENCL_VERSION_${VERSION})
+    else()
+      CHECK_SYMBOL_EXISTS(
+        CL_VERSION_${VERSION}
+        "${OpenCL_INCLUDE_DIR}/CL/cl.h"
+        OPENCL_VERSION_${VERSION})
+    endif()
+
+    if(OPENCL_VERSION_${VERSION})
+      string(REPLACE "_" "." VERSION "${VERSION}")
+      set(OpenCL_VERSION_STRING ${VERSION} PARENT_SCOPE)
+      string(REGEX MATCHALL "[0-9]+" version_components "${VERSION}")
+      list(GET version_components 0 major_version)
+      list(GET version_components 1 minor_version)
+      set(OpenCL_VERSION_MAJOR ${major_version} PARENT_SCOPE)
+      set(OpenCL_VERSION_MINOR ${minor_version} PARENT_SCOPE)
+      break()
+    endif()
+  endforeach()
+  CMAKE_POP_CHECK_STATE()
+endfunction()
+
+find_path(OpenCL_INCLUDE_DIR
+  NAMES
+    CL/cl.h OpenCL/cl.h OpenCL/opencl.h
+  PATHS
+    ENV "PROGRAMFILES(X86)"
+    ENV AMDAPPSDKROOT
+    ENV INTELOCLSDKROOT
+    ENV NVSDKCOMPUTE_ROOT
+    ENV CUDA_PATH
+    ENV ATISTREAMSDKROOT
+  PATH_SUFFIXES
+    include
+    OpenCL/common/inc
+    "AMD APP/include")
+
+_FIND_OPENCL_VERSION()
+
+if(WIN32)
+  if(CMAKE_SIZEOF_VOID_P EQUAL 4)
+    find_library(OpenCL_LIBRARY
+      NAMES OpenCL
+      PATHS
+        ENV "PROGRAMFILES(X86)"
+        ENV AMDAPPSDKROOT
+        ENV INTELOCLSDKROOT
+        ENV CUDA_PATH
+        ENV NVSDKCOMPUTE_ROOT
+        ENV ATISTREAMSDKROOT
+      PATH_SUFFIXES
+        "AMD APP/lib/x86"
+        lib/x86
+        lib/Win32
+        OpenCL/common/lib/Win32)
+  elseif(CMAKE_SIZEOF_VOID_P EQUAL 8)
+    find_library(OpenCL_LIBRARY
+      NAMES OpenCL
+      PATHS
+        ENV "PROGRAMFILES(X86)"
+        ENV AMDAPPSDKROOT
+        ENV INTELOCLSDKROOT
+        ENV CUDA_PATH
+        ENV NVSDKCOMPUTE_ROOT
+        ENV ATISTREAMSDKROOT
+      PATH_SUFFIXES
+        "AMD APP/lib/x86_64"
+        lib/x86_64
+        lib/x64
+        OpenCL/common/lib/x64)
+  endif()
+else()
+  find_library(OpenCL_LIBRARY
+    NAMES OpenCL)
+endif()
+
+set(OpenCL_LIBRARIES ${OpenCL_LIBRARY})
+set(OpenCL_INCLUDE_DIRS ${OpenCL_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(
   OpenCL
-  DEFAULT_MSG
-  OPENCL_LIBRARY OPENCL_INCLUDE_DIR)
-
-if(OPENCL_FOUND)
-  set(OPENCL_LIBRARIES ${OPENCL_LIBRARY})
-else(OPENCL_FOUND)
-  set(OPENCL_LIBRARIES)
-endif(OPENCL_FOUND)
+  FOUND_VAR OpenCL_FOUND
+  REQUIRED_VARS OpenCL_LIBRARY OpenCL_INCLUDE_DIR
+  VERSION_VAR OpenCL_VERSION_STRING)
 
 mark_as_advanced(
-  OPENCL_INCLUDE_DIR
-  OPENCL_LIBRARY
-  )
-
+  OpenCL_INCLUDE_DIR
+  OpenCL_LIBRARY)
